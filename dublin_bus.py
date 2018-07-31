8c64e7b34c97ed7052f99f871053910cdd372218
Sam <somahony@shimmersensing.com>
2018-07-31 09:36:01 +0100

    Now parses individual bus stop and has GUI window with Tkinter

--- dublin_bus.py
+++ dublin_bus.py
@@ -1,8 +1,38 @@
 import json
 import requests
-from pprint import pprint as prettyprint
+# from pprint import pprint
+from tkinter import *
+ 
+def clicked():
+	# lbl.configure(text="Button was clicked !!")
+	window.destroy()
 
-response = requests.get("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=7602&format=json")
+
+# Get Dublin Bus information
+stopID = "182"
+response = requests.get(''.join(["https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=",\
+	stopID,"&format=json"]))
 json_data = response.json()
 
-prettyprint(json_data)
+# Build a string with the retrieved information
+# pprint(json_data)
+data = ""
+bus_count = int(json_data["numberofresults"])
+for i in range(0,bus_count):
+	route = json_data["results"][i]["route"]
+	dueTime = json_data["results"][i]["duetime"]
+	data += "%s is due in %s minutes\n" % (route, dueTime)
+	# print "%s is due in %s minutes" % (route, dueTime)
+
+# Set up the user interface window
+window = Tk()
+window.title("Dublin Bus times")
+# window.geometry('350x200')
+ 
+lbl = Label(window, text=data, font=("Arial", 20))
+lbl.grid(column=0, row=0)
+
+btn = Button(window, text="Close", command=clicked)
+btn.grid(column=0, row=1)
+ 
+window.mainloop()
